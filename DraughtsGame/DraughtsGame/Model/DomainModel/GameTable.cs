09055c05e13b7165a10016fe035ecb154830d09c
alex.cracanel <alex.cracanel@gmail.com>
2016-03-29 22:29:03 +0300

    View almost ready

---
+++ DraughtsGame/DraughtsGame/Model/DomainModel/GameTable.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraughtsGame.Model.DomainModel
+{
+    
+    public class GameTable
+    {
+        private const int NUMBER_OF_LINES = 8;
+        private const int NUMBER_OF_COLUMNS = 8; 
+
+        private  GameUtil.CellState[,] gameTable;
+
+        public GameTable()
+        {
+            InitializeGameTable();
+        }
+
+        private void InitializeGameTable()
+        {
+            gameTable = new GameUtil.CellState[NUMBER_OF_LINES, NUMBER_OF_COLUMNS];
+
+            for (int i = 0; i < NUMBER_OF_LINES; i++)
+            {
+                for (int j = 0; j < NUMBER_OF_COLUMNS; j++)
+                {
+                    gameTable[i, j] = GameUtil.CellState.UNITIALIZED;
+                }
+            }
+
+            InitializeBlackQueen();
+            InitializeRedQueen();
+        }
+
+        private void InitializeBlackQueen()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j=((i+1)%2==0)?1:0;j < NUMBER_OF_COLUMNS; j+=2)
+                {
+                    gameTable[i, j] = GameUtil.CellState.BLACK;
+                } 
+            }
+        }
+
+        private void InitializeRedQueen()
+        {
+            for (int i = 5; i < NUMBER_OF_LINES; i++)
+            {
+                for (int j = ((i + 1) % 2 == 0) ? 0 : 1; j < NUMBER_OF_COLUMNS; j += 2)
+                {
+                    gameTable[i, j] = GameUtil.CellState.RED;
+                }
+            }
+        }
+
+        public void GetCellStateAtPosition(int row,int column, GameUtil.CellState cellState)
+        {
+            gameTable[row, column] = cellState;
+        }
+
+        public GameUtil.CellState GetCellStateAtPosition(int row,int column) 
+        {
+            return gameTable[row, column];
+        }
+
+        public void RestartGameTable()
+        {
+            InitializeGameTable();
+        }
+
+        public int GetNumberOfLines()
+        {
+            return NUMBER_OF_LINES;
+        }
+
+        public int GetNumberOfColumn()
+        {
+            return NUMBER_OF_COLUMNS;
+        }
+
+    }
+}
---
+++ DraughtsGame/DraughtsGame/Model/DomainModel/GameUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraughtsGame.Model.DomainModel
+{
+    public class GameUtil
+    {
+        public enum CellState
+        {
+            UNITIALIZED,
+            RED,
+            BLACK,
+            RED_KING,
+            BLACK_KING
+        }
+
+        public enum GameState
+        {
+            UNITIALIZED,
+            IN_GAME,
+            FINISHED
+        }
+
+        public enum CurrentPlayer
+        {
+            RED_PLAYER,
+            BLACK_PLAYER
+        }
+    }
+}
